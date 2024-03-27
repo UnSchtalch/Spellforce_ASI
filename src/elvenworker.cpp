fdb3a95530d6052e97744a65f143ec684eb80b07
UnSchtalch <shadow.93.04@gmail.com>
2024-03-27 20:34:01 +0300

    Every worker now can deliver iron to corresponding HQ

--- src/elvenworker.cpp
+++ src/elvenworker.cpp
@@ -3,6 +3,14 @@
 
 unsigned int JOB_SELECT_EXEC;
 unsigned int JOB_SELECT_FAIL;
+unsigned int JOB_SELECT_FAIL_2;
+
+unsigned int (__thiscall *get_figure_xdata)(void *, unsigned int, unsigned int);
+unsigned int (__thiscall *prepare_building_mask)(void *, unsigned short, unsigned short, unsigned short, unsigned short);
+unsigned int (__thiscall *query_entities_in_radius)(void *,unsigned int *, unsigned short);
+unsigned int (__thiscall *init_building_mask)(void *, unsigned int, unsigned int, unsigned int);
+unsigned int (__thiscall *specify_building)(void *, unsigned short);
+unsigned int (__thiscall *get_next_building_from_mask)(void *);
 
 unsigned short calculate_norm(short x, short y)
 {
@@ -15,27 +23,19 @@ unsigned short calculate_norm(short x, short y)
 
 unsigned int __thiscall find_closest_building(unsigned int *_this, unsigned int figure_id, unsigned short building_type)
 {
-    unsigned int (__thiscall *get_figure_xdata)(void *, unsigned int, unsigned int) = ASI::AddrOf(0x2FE3e8);
-    unsigned int (__thiscall *prepare_building_mask)(void *, unsigned short, unsigned short, unsigned short, unsigned short) = ASI::AddrOf(0x318290);
-    unsigned int (__thiscall *query_entities_in_radius)(void *,unsigned int *, unsigned short) = ASI::AddrOf(0x3195D0);
-    unsigned int (__thiscall *init_building_mask)(void*, unsigned int, unsigned int, unsigned int) = ASI::AddrOf(0x31A640);
-    unsigned int (__thiscall *specify_building)(void *, unsigned short) = ASI::AddrOf(0x31A6C0);
-    unsigned int (__thiscall *get_next_building_from_mask)(void *) = ASI::AddrOf(0x318CA0);
-
-    unsigned int figure_coords = * (unsigned int *)(*(void **)((int)_this + 0x24) + figure_id * 0x2b3 + 0x2);
+    unsigned int local_c = (unsigned int)(*(void **)((int)_this + 0x10));
+    unsigned int figure_coords = * (unsigned int *)(*(void **)((int)_this + 0x20) + figure_id * 0x2b3 + 0x2);
     unsigned short figure_x = figure_coords & 0xffff;
     unsigned short figure_y = figure_coords >> 0x10;
-    unsigned char mask[128];
+    unsigned char mask[0x52];
     unsigned short building_id = 0;
     unsigned short last_norm = 0xFFFF;
     unsigned short best_id = 0;
-
-    prepare_building_mask (&mask, 0,0, 0x3ff, 0x3ff);
-    unsigned int local_c = (unsigned int)(*(void **)((int)_this + 0x10));
-    init_building_mask(&mask, *(unsigned int *)((int)local_c + 0xc), *(unsigned int *)((int)local_c + 0x3c), *(unsigned int *)((int)local_c + 0x40));
-    query_entities_in_radius(&mask, &figure_coords, 0x1f); //hardcode the radius
-    specify_building(&mask, building_type);
-    building_id = get_next_building_from_mask(&mask);
+    prepare_building_mask (mask, 0,0, 0x3ff, 0x3ff);
+    init_building_mask(mask, *(unsigned int *)((int)local_c + 0xc), *(unsigned int *)((int)local_c + 0x3c), *(unsigned int *)((int)local_c + 0x40));
+    query_entities_in_radius(mask, &figure_coords, 0x1f); //hardcode the radius
+    specify_building(mask, building_type);
+    building_id = get_next_building_from_mask(mask);
     while (building_id != 0)
     {
 
@@ -49,7 +49,7 @@ unsigned int __thiscall find_closest_building(unsigned int *_this, unsigned int 
             last_norm = calculate_norm(dx, dy);
             best_id = building_id;
         }
-        building_id = get_next_building_from_mask(&mask);
+        building_id = get_next_building_from_mask(&mask[0]);
     }
     return best_id;
 }
@@ -58,15 +58,31 @@ unsigned short __attribute__((no_caller_saved_registers, thiscall)) worker_miner
 {
     unsigned int local_c = *(unsigned int *)((unsigned int)_this + 0x20) + param_1;
     unsigned char race_id = *(unsigned char *)(local_c + 0x1a);
+    if (race_id == 0x01)
+    {
+        return find_closest_building(_this, figure_id, 0x02);
+    }
+    if (race_id == 0x02)
+    {
+        return find_closest_building(_this, figure_id, 0x29);
+    }
     if (race_id  == 0x03)
     {
-        //void * (__thiscall *building_get_race)(void *, unsigned int) = ASI::AddrOf(0x2C6820);
-    /*    unsigned short (__thiscall *get_closest_building_id)(void *, unsigned int, unsigned int, unsigned int, unsigned int) = ASI::AddrOf(0x2D97D0);
-        unsigned short building_id = get_closest_building_id (*(void **)((int)_this + 0x10),
-                                   *(unsigned int *)((unsigned int)*(unsigned short *)(local_c + 0x14) * 0x34 + 4 + *(int *)((int)_this + 0xc)), 0x14, 
-                                   (unsigned int)*(unsigned short *)(local_c + 0x1c),0x19);*/
         return find_closest_building(_this, figure_id, 0x14);
     }
+    if (race_id == 0x04)
+    {
+        return find_closest_building(_this, figure_id, 0x49);
+    }
+    if (race_id == 0x05)
+    {
+        return find_closest_building(_this, figure_id, 0x3A);
+    }
+    if (race_id == 0x06)
+    {
+        return find_closest_building(_this, figure_id, 0x55);
+    }
+
     return 0;
 }
 
@@ -74,6 +90,8 @@ unsigned short __attribute__((no_caller_saved_registers, thiscall)) worker_miner
 void __declspec(naked) worker_miner_hook_beta()
 {
     asm(
+        "cmpb $0x2, %%al        \n\t"
+        "jne 2f                 \n\t"
         "mov %%esi, %%ecx       \n\t"
         "push %%edi             \n\t"
         "push -0x4(%%ebp)       \n\t" //iVar7
@@ -81,22 +99,34 @@ void __declspec(naked) worker_miner_hook_beta()
         "movzw %%ax, %%ebx      \n\t"
         "test %%bx, %%bx        \n\t"
         "jne 1f                 \n\t"
-        "mov 0x24(%%esi), %%ecx \n\t"
         "jmp *%1                \n\t"
-        "1: jmp *%2             \n\t":
-       :"i"(worker_miner_find_hq), "o"(JOB_SELECT_FAIL), "o"(JOB_SELECT_EXEC));
+        "1: jmp *%2             \n\t"
+        "2: jmp *%3             \n\t":
+       :"i"(worker_miner_find_hq), "o"(JOB_SELECT_FAIL), "o"(JOB_SELECT_EXEC), "o"(JOB_SELECT_FAIL_2));
 }
 
 void hookBetaVersion()
 {
+
+    get_figure_xdata = ASI::AddrOf(0x2FE3e8);
+    prepare_building_mask = ASI::AddrOf(0x318290);
+    query_entities_in_radius = ASI::AddrOf(0x3195D0);
+    init_building_mask = ASI::AddrOf(0x31A640);
+    specify_building = ASI::AddrOf(0x31A6C0);
+    get_next_building_from_mask = ASI::AddrOf(0x318CA0);
+
     //FIXME!!!
 	JOB_SELECT_EXEC = ASI::AddrOf(0x2EFF42);
-	JOB_SELECT_FAIL = ASI::AddrOf(0x2F0009);
-	ASI::MemoryRegion mreg(ASI::AddrOf(0x2F0004), 5); //building selector
+	JOB_SELECT_FAIL = ASI::AddrOf(0x2F0004);
+    JOB_SELECT_FAIL_2 = ASI::AddrOf(0x2EFEB1);
+	ASI::MemoryRegion mreg(ASI::AddrOf(0x2EFEA9), 8); //building selector
 
 	ASI::BeginRewrite(mreg);
-        *(unsigned char*)(ASI::AddrOf(0x2F0004)) = 0xE9;   // jmp instruction
-        *(int*)(ASI::AddrOf(0x2F0005)) = (int)(&worker_miner_hook_beta) - ASI::AddrOf(0x2F0009);
+        *(unsigned char*)(ASI::AddrOf(0x2EFEA9)) = 0xE9;   // jmp instruction
+        *(int*)(ASI::AddrOf(0x2EFEAA)) = (int)(&worker_miner_hook_beta) - ASI::AddrOf(0x2EFEAE);
+        *(unsigned char*)(ASI::AddrOf(0x2EFEAE)) = 0x90;   // nop trail
+        *(unsigned char*)(ASI::AddrOf(0x2EFEAF)) = 0x90;   // nop trail
+        *(unsigned char*)(ASI::AddrOf(0x2EFEB0)) = 0x90;   // nop trail
     ASI::EndRewrite(mreg);
 }
 
